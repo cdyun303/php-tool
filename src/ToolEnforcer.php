7a8979b9962e5bacad57709b44372d12551c076c
cdyun <121625706@qq.com>
2025-11-06 15:49:31 +0800

    cdyun

--- src/ToolEnforcer.php
+++ src/ToolEnforcer.php
@@ -331,4 +331,84 @@ class ToolEnforcer
         return $result;
     }
 
+    /**
+     * 清除指定文件夹下文件
+     * @param $dir_name
+     * @return bool
+     * @author cdyun(121625706@qq.com)
+     */
+    public static function deleteDirFile($dir_name): bool
+    {
+        $result = false;
+        if (is_dir($dir_name)) {
+            if ($handle = opendir($dir_name)) {
+                while (false !== ($item = readdir($handle))) {
+                    if ($item != '.' && $item != '..') {
+                        if (is_dir($dir_name . DIRECTORY_SEPARATOR . $item)) {
+                            self::deleteDirFile($dir_name . DIRECTORY_SEPARATOR . $item);
+                        } else {
+                            unlink($dir_name . DIRECTORY_SEPARATOR . $item);
+                        }
+                    }
+                }
+                closedir($handle);
+                if (rmdir($dir_name)) {
+                    $result = true;
+                }
+            }
+        }
+        return $result;
+    }
+
+    /**
+     * 搜索指定路径下的【直系】文件夹
+     * @param string $path
+     * @return array
+     * @author cdyun(121625706@qq.com)
+     */
+    public static function scanFolder(string $path): array
+    {
+        if (!is_dir($path)) return [];
+        // 兼容各操作系统
+        $path = rtrim(str_replace('\\', '/', $path), '/') . '/';
+        $result = [];
+        $files = scandir($path);
+        foreach ($files as $vo) {
+            if ($vo != '.' && $vo != '..' && is_dir($path . '/' . $vo)) {
+                $result[] = $vo;
+            }
+        }
+        return $result;
+    }
+
+    /**
+     * 搜索文件夹下全部文件，指定扩展名，暂时不支持中文文件名
+     * @param string $path
+     * @param array $ext
+     * @return array
+     * @author cdyun(121625706@qq.com)
+     */
+    public static function scanFile(string $path, array $ext = ['html']): array
+    {
+        if (!is_dir($path))
+            return array();
+        // 兼容各操作系统
+        $path = rtrim(str_replace('\\', '/', $path), '/') . '/';
+        $result = array();
+        $files = scandir($path);
+        foreach ($files as $vo) {
+            if ($vo != '.' && $vo != '..') {
+                $vo = iconv("GBK  ", "utf-8", $vo);
+                if (is_dir($path . '/' . $vo)) {
+                    $result = array_merge($result, self::scanFile($path . '/' . $vo, $ext));
+                } else {
+                    if (in_array(pathinfo($vo, PATHINFO_EXTENSION), $ext)) {
+                        $result[] = basename($vo);
+                    }
+                }
+            }
+        }
+        return $result;
+    }
+
 }
